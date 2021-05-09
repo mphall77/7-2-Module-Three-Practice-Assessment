2b4e580a62fe9e8704a3a5454c70a51959146969
Marsha Hall <mphall77@gmail.com>
2021-05-09 17:17:17 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="app">
       <main>
-        <h1>Hello, world!</h1>
+        <NavBar />
+        <h1>Welcome To My Pokemon App!</h1>
+        {/* <Switch>
+          <Route exact path={"/"} component={Home} />
+          <Route path={"/pokemon"} component={Pokemon} />
+          <Route path={"locations"} components={Locations} />
+          <Route path={"berries"} components={Berries} />
+        </Switch> */}
       </main>
     </div>
   );
