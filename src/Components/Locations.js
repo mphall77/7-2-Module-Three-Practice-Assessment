021fcc540deee5bdcf07070de6030bc85f428b95
Marsha Hall <mphall77@gmail.com>
2021-05-09 23:29:33 -0400

    toggle button works

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class Locations extends Component {
-	state = { locations: [], displayText: true };
+	state = { locations: [], showText: false };
 
 	loadLocations = async () => {
 		try {
@@ -19,28 +19,24 @@ export default class Locations extends Component {
 		this.loadLocations();
 	}
 
-	toggleButton = () => {
-		//    locations.map(location => {
-		//        <ul>
-		//            <li key={location.url}>
-		//                {location.name}
-		//             </li>
-		//         </ul>
-		//     })
+    toggleButton = () => {
+         this.setState((prevState) => ({ showText: !prevState.showText }));
 	};
 
 	render() {
-		const { locations } = this.state;
+		const { locations, showText } = this.state;
 
 		return (
 			<div>
 				<h2>List of Locations</h2>
-                    {/* <button onClick={()=>this.toggleButton()}>Show Locations</button> */}
-				<ul>
-					{locations.map((location) => {
-						return(<li key={location.name}>{location.name}</li>)
+				<button onClick={() => this.toggleButton()}>
+					{showText ? "Hide Locations" : "Show Locations"}
+				</button>
+				{showText && <ul>
+					{this.state.locations.map((location) => {
+						return <li key={location.name}>{location.name}</li>;
 					})}
-				</ul>
+				</ul>}
 			</div>
 		);
 	}
